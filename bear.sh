d49368d7e6b30e71aa51568b247cc91c096eb55c
nyquil1x <gamboa.family.8@gmail.com>
2024-09-27 11:50:42 -0700

    changed script name to bearmessenger

--- bear.sh
+++ bear.sh
@@ -1,18 +1,26 @@
 #!/bin/bash
 
 install_script() {
-    SCRIPT_NAME="bear"
-    TARGET_PATH="/usr/local/bin/$SCRIPT_NAME"
+    SCRIPT_NAME="bearmessenger"
+    
+    # Detect if this is an ARM-based Mac (M1/M2) or Intel-based
+    if [[ $(uname -m) == 'arm64' ]]; then
+        INSTALL_DIR="/opt/homebrew/bin"
+    else
+        INSTALL_DIR="/usr/local/bin"
+    fi
+
+    TARGET_PATH="$INSTALL_DIR/$SCRIPT_NAME"
     
     if [[ $0 != "$TARGET_PATH" ]]; then
-        echo "The script is not in /usr/local/bin. Attempting to move it there..."
+        echo "The script is not in $INSTALL_DIR. Attempting to move it there..."
         sudo mv "$0" "$TARGET_PATH" && sudo chmod +x "$TARGET_PATH"
         
         if [[ $? -eq 0 ]]; then
-            echo "Successfully moved to /usr/local/bin. You can now run 'bear' from anywhere."
+            echo "Successfully moved to $INSTALL_DIR. You can now run '$SCRIPT_NAME' from anywhere."
             exit 0
         else
-            echo "Failed to move the script to /usr/local/bin."
+            echo "Failed to move the script to $INSTALL_DIR."
             exit 1
         fi
     fi
